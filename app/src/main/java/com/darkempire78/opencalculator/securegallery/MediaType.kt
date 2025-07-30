69e64dde17eeda0ba3a9d229fac407eb5902c136
Dustin Hartlyn <dustin.hartlyn@gmail.com>
2025-07-29 22:36:00 -0700

    fixing more errors

--- app/src/main/java/com/darkempire78/opencalculator/securegallery/MediaType.kt
+++
@@ -1,9 +0,0 @@
-package com.darkempire78.opencalculator.securegallery
-
-/**
- * Enum to represent different types of media in the secure gallery
- */
-enum class MediaType {
-    PHOTO,
-    VIDEO
-}
